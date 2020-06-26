9bda307c7f00415e0d1653299ba6f109527b92c2
Dzmitry Yeutukhovich <n0nsenskype@gmail.com>
2020-06-26 11:47:35 +0300

    Fix print method

--- src/com/senla/training/yeutukhovich/bookstore/util/printer/EntityPrinter.java
+++ src/com/senla/training/yeutukhovich/bookstore/util/printer/EntityPrinter.java
@@ -8,12 +8,13 @@ public class EntityPrinter {
 
     public static void printEntities(List<String> entityStrings) {
 
-        if (entityStrings != null && !entityStrings.isEmpty()) {
-            System.out.println(MessageConstant.DATA_WAS_FOUND.getMessage());
-            System.out.println();
-            entityStrings.forEach(System.out::println);
-        } else {
+        if (entityStrings == null || entityStrings.isEmpty()) {
             System.out.println(MessageConstant.NO_DATA_WAS_FOUND.getMessage());
+            return;
         }
+
+        System.out.println(MessageConstant.DATA_WAS_FOUND.getMessage());
+        System.out.println();
+        entityStrings.forEach(System.out::println);
     }
 }
