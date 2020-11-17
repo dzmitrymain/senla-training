d7f933ad95c6c7f69b2abdee433b4fa71afe35bf
Dzmitry Yeutukhovich <n0nsenskype@gmail.com>
2020-11-17 15:40:41 +0300

    Fix db scripts.

--- database/DDL-create-schema-scooter_rental.sql
+++ database/DDL-create-schema-scooter_rental.sql
@@ -4,7 +4,7 @@ USE scooter_rental;
 
 CREATE TABLE scooter_rental.`locations`
 (
-    `id`   BIGINT         NOT NULL AUTO_INCREMENT,
+    `id`   BIGINT      NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(45) NOT NULL,
     CONSTRAINT `pk_locations_id` PRIMARY KEY (`id`),
     UNIQUE INDEX `unique_index_locations_name` (`name` ASC) VISIBLE
@@ -12,21 +12,21 @@ CREATE TABLE scooter_rental.`locations`
 
 CREATE TABLE scooter_rental.`users`
 (
-    `id`            BIGINT          NOT NULL AUTO_INCREMENT,
+    `id`            BIGINT       NOT NULL AUTO_INCREMENT,
     `username`      VARCHAR(255) NOT NULL,
     `password`      VARCHAR(255) NOT NULL,
     `role`          VARCHAR(45)  NOT NULL,
-    `creation_date` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `enabled`       TINYINT      NOT NULL,
+    `creation_date` DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `enabled`       BIT          NOT NULL DEFAULT 1,
     CONSTRAINT `pk_users_id` PRIMARY KEY (`id`),
     UNIQUE INDEX `unique_index_users_username` (`username` ASC) VISIBLE
 );
 
 CREATE TABLE scooter_rental.`profiles`
 (
-    `id`           BIGINT          NOT NULL AUTO_INCREMENT,
-    `user_id`      BIGINT          NOT NULL,
-    `location_id`  BIGINT          NOT NULL,
+    `id`           BIGINT       NOT NULL AUTO_INCREMENT,
+    `user_id`      BIGINT       NOT NULL,
+    `location_id`  BIGINT       NOT NULL,
     `name`         VARCHAR(255) NOT NULL,
     `surname`      VARCHAR(255) NOT NULL,
     `email`        VARCHAR(255) NOT NULL,
@@ -39,19 +39,19 @@ CREATE TABLE scooter_rental.`profiles`
 
 CREATE TABLE scooter_rental.`models`
 (
-    `id`    BIGINT          NOT NULL AUTO_INCREMENT,
-    `model` VARCHAR(255) NOT NULL,
+    `id`    BIGINT       NOT NULL AUTO_INCREMENT,
+    `name`  VARCHAR(255) NOT NULL,
     `range` SMALLINT     NOT NULL,
     `speed` SMALLINT     NOT NULL,
     `power` SMALLINT     NOT NULL,
     CONSTRAINT `pk_models_id` PRIMARY KEY (`id`),
-    UNIQUE INDEX `unique_index_models_model` (`model` ASC) VISIBLE
+    UNIQUE INDEX `unique_index_models_name` (`name` ASC) VISIBLE
 );
 
 CREATE TABLE scooter_rental.`spots`
 (
-    `id`           BIGINT         NOT NULL AUTO_INCREMENT,
-    `location_id`  BIGINT         NOT NULL,
+    `id`           BIGINT      NOT NULL AUTO_INCREMENT,
+    `location_id`  BIGINT      NOT NULL,
     `phone_number` VARCHAR(13) NULL,
     `coordinates`  POINT       NOT NULL,
     CONSTRAINT `pk_spots_id` PRIMARY KEY (`id`)
@@ -59,30 +59,30 @@ CREATE TABLE scooter_rental.`spots`
 
 CREATE TABLE scooter_rental.`scooters`
 (
-    `id`                       BIGINT       NOT NULL AUTO_INCREMENT,
-    `model_id`                 BIGINT       NOT NULL,
-    `spot_id`                  BIGINT       NOT NULL,
-    `begin_operation_date`     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `total_distance_travelled` INT       NOT NULL DEFAULT 0,
+    `id`                   BIGINT   NOT NULL AUTO_INCREMENT,
+    `model_id`             BIGINT   NOT NULL,
+    `spot_id`              BIGINT   NOT NULL,
+    `begin_operation_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT `pk_scooters_id` PRIMARY KEY (`id`)
 );
 
 CREATE TABLE scooter_rental.`rates`
 (
-    `id`               BIGINT            NOT NULL AUTO_INCREMENT,
-    `model_id`         BIGINT            NOT NULL,
+    `id`               BIGINT         NOT NULL AUTO_INCREMENT,
+    `model_id`         BIGINT         NOT NULL,
     `per_hour`         DECIMAL(15, 2) NOT NULL,
     `weekend_per_hour` DECIMAL(15, 2) NOT NULL,
+    `creation_date`    DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT `pk_rates_id` PRIMARY KEY (`id`)
 );
 
 CREATE TABLE scooter_rental.`passes`
 (
-    `id`                BIGINT            NOT NULL AUTO_INCREMENT,
-    `user_id`           BIGINT            NOT NULL,
-    `model_id`          BIGINT            NOT NULL,
-    `start_date`        TIMESTAMP      NOT NULL,
-    `expired_date`      TIMESTAMP      NOT NULL,
+    `id`                BIGINT         NOT NULL AUTO_INCREMENT,
+    `user_id`           BIGINT         NOT NULL,
+    `model_id`          BIGINT         NOT NULL,
+    `start_date`        DATETIME       NOT NULL,
+    `expired_date`      DATETIME       NOT NULL,
     `total_minutes`     INT            NOT NULL,
     `remaining_minutes` INT            NOT NULL,
     `price`             DECIMAL(15, 2) NOT NULL,
@@ -91,25 +91,25 @@ CREATE TABLE scooter_rental.`passes`
 
 CREATE TABLE scooter_rental.`discounts`
 (
-    `id`         BIGINT           NOT NULL AUTO_INCREMENT,
-    `model_id`   BIGINT           NOT NULL,
-    `start_date` TIMESTAMP     NOT NULL,
-    `end_date`   TIMESTAMP     NOT NULL,
+    `id`         BIGINT        NOT NULL AUTO_INCREMENT,
+    `model_id`   BIGINT        NOT NULL,
+    `start_date` DATETIME      NOT NULL,
+    `end_date`   DATETIME      NOT NULL,
     `discount`   DECIMAL(3, 2) NOT NULL,
     CONSTRAINT `pk_discounts_id` PRIMARY KEY (`id`)
 );
 
 CREATE TABLE scooter_rental.`rents`
 (
-    `id`                 BIGINT            NOT NULL AUTO_INCREMENT,
-    `user_id`            BIGINT            NOT NULL,
-    `scooter_id`         BIGINT            NOT NULL,
-    `state`              VARCHAR(45)    NOT NULL,
-    `start_date`         TIMESTAMP      NOT NULL,
-    `end_date`           TIMESTAMP      NOT NULL,
+    `id`                 BIGINT         NOT NULL AUTO_INCREMENT,
+    `user_id`            BIGINT         NOT NULL,
+    `scooter_id`         BIGINT         NOT NULL,
+    `active`             BIT            NOT NULL DEFAULT 1,
+    `start_date`         DATETIME       NOT NULL,
+    `expired_date`       DATETIME       NOT NULL,
     `payment_type`       VARCHAR(45)    NOT NULL,
     `price`              DECIMAL(15, 2) NOT NULL DEFAULT 0.00,
-    `return_date`        TIMESTAMP      NULL,
+    `return_date`        DATETIME       NULL,
     `distance_travelled` INT            NULL,
     `overtime_penalty`   DECIMAL(15, 2) NULL,
     CONSTRAINT `pk_rents_id` PRIMARY KEY (`id`)
@@ -117,14 +117,14 @@ CREATE TABLE scooter_rental.`rents`
 
 CREATE TABLE scooter_rental.`reviews`
 (
-    `id`            BIGINT           NOT NULL AUTO_INCREMENT,
-    `user_id`       BIGINT           NOT NULL,
-    `model_id`      BIGINT           NULL,
+    `id`            BIGINT        NOT NULL AUTO_INCREMENT,
+    `profile_id`       BIGINT        NOT NULL,
+    `model_id`      BIGINT        NULL,
     `score`         TINYINT       NOT NULL,
     `opinion`       VARCHAR(2000) NULL,
-    `creation_date` TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `creation_date` DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT `pk_reviews_id` PRIMARY KEY (`id`),
-    UNIQUE INDEX `unique_index_reviews_user_id` (`user_id` ASC) VISIBLE
+    UNIQUE INDEX `unique_index_reviews_profile_id` (`profile_id` ASC) VISIBLE
 );
 
 ALTER TABLE scooter_rental.`profiles`
@@ -208,9 +208,9 @@ ALTER TABLE scooter_rental.`rents`
     ADD INDEX `index_rents_scooter_id` (`scooter_id` ASC) VISIBLE;
 
 ALTER TABLE scooter_rental.`reviews`
-    ADD CONSTRAINT `fk_reviews_users`
-        FOREIGN KEY (`user_id`)
-            REFERENCES scooter_rental.`users` (`id`)
+    ADD CONSTRAINT `fk_reviews_profiles`
+        FOREIGN KEY (`profile_id`)
+            REFERENCES scooter_rental.`profiles` (`id`)
             ON DELETE RESTRICT
             ON UPDATE RESTRICT,
     ADD CONSTRAINT `fk_reviews_models`
@@ -218,7 +218,7 @@ ALTER TABLE scooter_rental.`reviews`
             REFERENCES scooter_rental.`models` (`id`)
             ON DELETE RESTRICT
             ON UPDATE RESTRICT,
-    ADD INDEX `index_reviews_user_id` (`user_id` ASC) VISIBLE,
+    ADD INDEX `index_reviews_user_id` (`profile_id` ASC) VISIBLE,
     ADD INDEX `index_reviews_models_id` (`model_id` ASC) VISIBLE;
 
 COMMIT;
---
+++ database/DML-init-schema-scooter_rental.sql
@@ -0,0 +1,45 @@
+USE `scooter_rental`;
+
+-- scooter_rental.locations
+INSERT INTO `scooter_rental`.`locations` (`id`, `name`)
+VALUES ('1', 'Minsk');
+
+-- scooter_rental.spots
+INSERT INTO `scooter_rental`.`spots` (`id`, `location_id`, `phone_number`, `coordinates`)
+VALUES ('1', '1', '+375291117161', POINT(53.952068, 27.664864))
+     , ('2', '1', '+375292227262', POINT(53.932336, 27.650894))
+     , ('3', '1', '+375293337363', POINT(53.908691, 27.549157));
+
+-- scooter_rental.models
+INSERT INTO `scooter_rental`.`models` (`id`, `name`, `range`, `speed`, `power`)
+VALUES ('1', 'XIAOMI m365', '25', '25', '250')
+     , ('2', 'XIAOMI m365 Pro', '40', '25', '350')
+     , ('3', 'Ninebot Max', '50', '25', '350');
+
+-- scooter_rental.scooters
+INSERT INTO `scooter_rental`.`scooters` (`id`, `model_id`, `spot_id`)
+VALUES ('1', '1', '1')
+     , ('2', '2', '1')
+     , ('3', '3', '1')
+     , ('4', '1', '2')
+     , ('5', '2', '2')
+     , ('6', '3', '2')
+     , ('7', '1', '3')
+     , ('8', '2', '3')
+     , ('9', '3', '3');
+
+-- scooter_rental.rates
+INSERT INTO `scooter_rental`.`rates` (`id`, `model_id`, `per_hour`, `weekend_per_hour`)
+VALUES ('1', '1', '6', '8')
+     , ('2', '2', '7', '9')
+     , ('3', '3', '8', '10');
+
+-- scooter_rental.users
+INSERT INTO `scooter_rental`.`users` (`id`, `username`, `password`, `role`)
+VALUES ('1', 'test', 'test', 'ADMIN');
+
+-- scooter_rental.profiles
+INSERT INTO `scooter_rental`.`profiles` (`id`, `user_id`, `location_id`, `name`, `surname`, `email`, `phone_number`)
+VALUES ('1', '1', '1', 'test', 'test', 'test', 'test');
+
+COMMIT;
