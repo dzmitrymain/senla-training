dc38ac2f169dea466d588c7d4db34a60983b92d1
Dzmitry Yeutukhovich <n0nsenskype@gmail.com>
2020-12-03 16:26:48 +0300

    Add javadocs to RentServiceImpl create, update methods.

--- scooter-rental-service/src/main/java/com/senla/training/yeutukhovich/scooterrental/service/rent/RentServiceImpl.java
+++ scooter-rental-service/src/main/java/com/senla/training/yeutukhovich/scooterrental/service/rent/RentServiceImpl.java
@@ -6,7 +6,9 @@ import com.senla.training.yeutukhovich.scooterrental.dao.scooter.ScooterDao;
 import com.senla.training.yeutukhovich.scooterrental.dao.user.UserDao;
 import com.senla.training.yeutukhovich.scooterrental.domain.Model;
 import com.senla.training.yeutukhovich.scooterrental.domain.Pass;
+import com.senla.training.yeutukhovich.scooterrental.domain.Rate;
 import com.senla.training.yeutukhovich.scooterrental.domain.Rent;
+import com.senla.training.yeutukhovich.scooterrental.domain.Scooter;
 import com.senla.training.yeutukhovich.scooterrental.domain.User;
 import com.senla.training.yeutukhovich.scooterrental.domain.type.PaymentType;
 import com.senla.training.yeutukhovich.scooterrental.dto.CreationRentDto;
@@ -111,6 +113,24 @@ public class RentServiceImpl implements RentService {
         return rentDtoMapper.map(rentToDelete);
     }
 
+    /**
+     * This method ends the {@link Rent} and apply changes in the database. And returns updated {@link Rent} mapped to
+     * {@link RentDto}.
+     * <p>
+     * It invokes {@link #findById(Long)} to find the current {@link Rent}.
+     * Then invokes {@link #checkUserMatch(User)} in  which it checks that the current {@link User} matches the one
+     * specified in the {@link Rent}.
+     * If {@link Rent} is expired then invokes {@link #calculateRentPrice(Model, long)} and the returned value is
+     * multiplied by a {@link #overtimeCoefficient} to get {@link Rent#getOvertimePenalty()}.
+     * If {@link Rent} is not expired then invokes {@link #createBonusPass(Rent, LocalDateTime, long)} and a bonus
+     * {@link Pass} is created and added to database, with unused minutes.
+     *
+     * @param id                of the {@link User} who ends the {@link Rent}
+     * @param distanceTravelled distance traveled by the {@link User} on the {@link Scooter}
+     * @return RentDto mapped from modified Rent
+     * @throws BusinessException     if Rent doesn't exist or Rent is already not active.
+     * @throws AccessDeniedException if the current User does not match the specified one
+     */
     @Override
     @Transactional
     public RentDto completeRent(Long id, @PositiveOrZero Integer distanceTravelled) {
@@ -142,6 +162,25 @@ public class RentServiceImpl implements RentService {
         return rentDtoMapper.map(rent);
     }
 
+    /**
+     * This method creates and adds a {@link Rent} object to the database. And returns the created object mapped
+     * to {@link RentDto}.
+     * <p>
+     * It invokes {@link UserService#findById(Long)} to find the {@link User}.
+     * Then invokes {@link #checkUserMatch(User)} in  which it checks that the current {@link User} matches the one
+     * specified in the {@link CreationRentDto}.
+     * Then invokes {@link ScooterService#findById(Long)} to find the {@link Scooter}.
+     * Then invokes {@link ScooterDao#findActiveRentScooters()} to check that {@link Scooter} is available.
+     * And invokes {@link #completePayment(CreationRentDto, Model, LocalDateTime)} in which, depending on
+     * the {@link CreationRentDto#getPaymentType()}, deducts the remaining minutes from the {@link User}'s {@link Pass}es,
+     * and calculates the {@link Rent} price according to
+     * the {@link Rate}.
+     *
+     * @param creationRentDto an object that contains information about User, rental Scooter, rental time, PaymentType
+     * @return RentDto mapped from created Rent
+     * @throws BusinessException     if User doesn't exist or Scooter doesn't exist or Scooter isn't available
+     * @throws AccessDeniedException if the current User does not match the specified one
+     */
     @Override
     @Transactional
     public RentDto create(@Valid CreationRentDto creationRentDto) {
